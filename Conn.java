bd9750be609347aec8fb4eaef3a4905e996cd1f2
Rushi2417 <127549955+Rushi2417@users.noreply.github.com>
2023-03-15 22:43:57 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,13 @@
+import java.sql.*;
+public class Conn {
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "Rushi@08");
+            s = c.createStatement();
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
